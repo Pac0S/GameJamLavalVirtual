19f72c258af9a05d0602fb72972aa2944a325b9a
Pac0o0 <paco.salomon@hrv-simulation.com>
2021-07-05 23:57:25 +0200

    New c++ project with Leap

---
+++ LeapTestProject/Source/LeapTestProject/LeapTestProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeapTestProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAPTESTPROJECT_API ALeapTestProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
