c49668f9e1c7a4c1428954d0eb11ee9f3a8ca4e8
NathanA14 <nathan.attia@me.com>
2021-07-06 03:03:44 +0200

    newVersionVRReady

---
+++ LeapTestProject/Intermediate/Build/Win64/LeapTestProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30038
+#include "C:/Users/HP Zbook17 G6/Documents/GitHub/GameJamLavalVirtual/LeapTestProject/Intermediate/Build/Win64/LeapTestProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
---
+++ LeapTestProject/Intermediate/Build/Win64/UE4Editor/Inc/LeapTestProject/LeapTestProjectGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LEAPTESTPROJECT_LeapTestProjectGameModeBase_generated_h
+#error "LeapTestProjectGameModeBase.generated.h already included, missing '#pragma once' in LeapTestProjectGameModeBase.h"
+#endif
+#define LEAPTESTPROJECT_LeapTestProjectGameModeBase_generated_h
+
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_SPARSE_DATA
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_RPC_WRAPPERS
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALeapTestProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ALeapTestProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ALeapTestProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/LeapTestProject"), NO_API) \
+	DECLARE_SERIALIZER(ALeapTestProjectGameModeBase)
+
+
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesALeapTestProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ALeapTestProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ALeapTestProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/LeapTestProject"), NO_API) \
+	DECLARE_SERIALIZER(ALeapTestProjectGameModeBase)
+
+
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALeapTestProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALeapTestProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALeapTestProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALeapTestProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALeapTestProjectGameModeBase(ALeapTestProjectGameModeBase&&); \
+	NO_API ALeapTestProjectGameModeBase(const ALeapTestProjectGameModeBase&); \
+public:
+
+
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALeapTestProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALeapTestProjectGameModeBase(ALeapTestProjectGameModeBase&&); \
+	NO_API ALeapTestProjectGameModeBase(const ALeapTestProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALeapTestProjectGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALeapTestProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALeapTestProjectGameModeBase)
+
+
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_12_PROLOG
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_SPARSE_DATA \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_RPC_WRAPPERS \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_INCLASS \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_SPARSE_DATA \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> LEAPTESTPROJECT_API UClass* StaticClass<class ALeapTestProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LeapTestProject_Source_LeapTestProject_LeapTestProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
